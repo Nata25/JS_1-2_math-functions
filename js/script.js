f303e253cf481fb4e330db0b350c40a16d7e60ba
nataivanova <natalyas.ivanova@gmail.com>
2016-10-31 01:00:28 +0200

    Function works with numbers

--- js/script.js
+++ js/script.js
@@ -19,62 +19,55 @@ function pow() {
       console.log("No values provided.");
   }
 
-  //
-  // else if ( (number == '') || (power == '') ) {
-  //     number = power = '1';
-  //     console.log("You didn't specify some values.");
-  //     console.log("Here's the default result:");
-  // }
-  //
-  // // If number is 0, return 0 and do no further calculations
-  // else if (number == 0) result = 0;
-
   else {
     result = calculatePow(number, power);
   }
 
-  console.log(number, 'raised to the power of',
-              power, 'equals', result);
+  console.log(number + ' to the power of ' +
+              power + ' equals ' + result);
 
 } // end of pow()
 
-
 // -------------------------------------
-// Helper functions
+// Helpers
 // -------------------------------------
 
 // Get and validate user input; only numbers are allowed
 // @param {string} param
+// @return {number} value
+
 function getParameters(param) {
-  var result = prompt('Enter the ' + param + ':', '1');
+  var input = prompt('Enter the ' + param + ':', '1');
   // Check if user entered a number; prompt again if not:
-  var checkResult = +result;
-  while ( (Number.isNaN(checkResult)) || (result.indexOf(' ') != -1) ) {
-    result = prompt('Only numbers, please. Enter the ' + param + ':', '1');
-    checkResult = +result;
+  var value = +input;
+  while ( (Number.isNaN(input)) || (input.indexOf(' ') != -1) ) {
+    input = prompt('Only numbers, please. Enter the ' + param + ':', '1');
+    value = +input;
   }
-  return result;
+  return value;
 }
 
-// Calculate any power of number != 0
-// @param {number} number > 0 or < 0
+// Calculate power of a number
+// @param {number} number >= 0 or =< 0
 // @param {number} power
+
 function calculatePow(number, power) {
-  // for absolute value of power
-  var absResult = absPow(number, Math.abs(power));
-  // based on negative/positive value of power
-  if (power > 0) return absResult;
-  else return 1 / absResult;
+
+    if (number == 0) return 0;
+
+    // for absolute value of power
+    var absResult = absPow(number, Math.abs(power));
+
+    // based on negative/positive value of power
+    if (power > 0) return absResult;
+    else return 1 / absResult;
 }
 
-// Recursively calculate power of number if power is positive
-// @param {number} number
+// Recursively calculate power of number
+// @param {number} number > 0
 // @param {number} power is natural >= 0
+
 function absPow(number, power) {
-    if (power == 0) {
-      return 1;
-    }
-    else {
-      return number * absPow(number, power - 1);
-    }
+    if (power == 0) return 1;
+    else return number * absPow(number, power - 1);
 }
