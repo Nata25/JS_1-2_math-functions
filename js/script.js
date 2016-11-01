40e650eb4eef7abbb32cf7ba5ab9e5547abe01a3
nataivanova <natalyas.ivanova@gmail.com>
2016-11-01 13:48:51 +0200

    Improve input validation and try to check for hex

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // -------------------------------------
 // JavaScript. HW 1
 // analogue of Math.pow()
+// pow is whole number
 // -------------------------------------
 
 var number = prompt("Enter number: ", 0);
 var power = prompt("Enter power:", 0);
 
-if ( (isNaN(+number)) || (isNaN(+power)) ) {
+if ( (!isNumeric(number)) || (!isNumeric(power)) ) {
     console.log("Invalid input");
 }
 else console.log(pow(number, power));
@@ -17,7 +18,17 @@ function pow(num, pw) {
     for (var i = 1; i<= Math.abs(pw); i++) {
         result *= num;
     }
+    if (pw < 0) result = 1/result;
 
-    if (pw > 0) return result;
-    return 1/result;
+    var decimal = parseInt(num, 16);
+    if (decimal.toString(16) == num) {
+        console.log("true");
+    }
+
+    return "0x" + result.toString(16);
+}
+
+// helper to manage empty string
+function isNumeric(n) {
+    return !isNaN(parseFloat(n));
 }
